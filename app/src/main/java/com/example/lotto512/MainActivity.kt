1481be080c1167a62023321867a9745b8e58ed28
M-J Kim <hohoily39@naver.com>
2021-05-20 14:46:51 +0900

    Open NameActivity

--- app/src/main/java/com/example/lotto512/MainActivity.kt
+++ app/src/main/java/com/example/lotto512/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.lotto512
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val cardConstellation = findViewById<CardView>(R.id.cardConstellation)
-
-
-        cardConstellation.setOnClickListener {
-            startActivity(Intent(this, ConstellationActivity::class.java))
+        findViewById<View>(R.id.Constellation_CardView).setOnClickListener {
+            startActivity(Intent(this,NameActivity::class.java))
         }
     }
 }
